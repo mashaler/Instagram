542de352685dc0dc643b25be20b1d98cc61be9f7
mashaler <mashalerolva@gmail.com>
2024-09-06 01:28:58 +0200

    added structs in the model

--- Instagram/Model/Comment.swift
+++ Instagram/Model/Comment.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct Comment {
+    
+    let user: User
+    let text: String
+    let uid: String
+
+    init(user: User, dictionary: [String: Any]) {
+        self.user = user
+        self.text = dictionary["text"] as? String ?? ""
+        self.uid = dictionary["uid"] as? String ?? ""
+    }
+    
+}
--- Instagram/Model/Message.swift
+++ Instagram/Model/Message.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct Message {
+    var fromId: String
+    var text: String
+    var toId: String
+    var timeStamp: Double
+    
+    init(dictionary: [String: Any]) {
+        fromId = dictionary["fromId"] as? String ?? ""
+        text = dictionary["text"] as? String ?? ""
+        toId = dictionary["toId"] as? String ?? ""
+        timeStamp = dictionary["timeStamp"] as? Double ?? 0
+    }
+    
+}
