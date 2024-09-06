7e177d50953e50426f601acfb8492c2d2d79d1c3
mashaler <mashalerolva@gmail.com>
2024-09-06 20:53:22 +0200

    added functionality for the users and user search

--- Instagram/User/UserProfilePhotoCell.swift
+++ Instagram/User/UserProfilePhotoCell.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import UIKit
+
+class UserProfilePhotoCell: UICollectionViewCell {
+    var post: Post? {
+        didSet {
+            guard let imageUrl = post?.imageUrl else { return }
+            photoImageView.loadImage(urlString: imageUrl)
+        }
+    }
+
+    let photoImageView: CustomImageView = {
+        let iv = CustomImageView()
+        iv.backgroundColor = .tertiarySystemBackground
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+
+    private func sharedInit() {
+        addSubview(photoImageView)
+        photoImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+    }
+}
