609d7f154c1470b3dba1640e8da87344b16a9729
mashaler <mashalerolva@gmail.com>
2024-09-06 20:59:06 +0200

    added code for the utilities

--- Instagram/Utilities/CustomeImageView.swift
+++ Instagram/Utilities/CustomeImageView.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import UIKit
+
+// image caching
+var imageCache = [String: UIImage]()
+
+var i = 0
+var j = 0
+
+
+class CustomImageView: UIImageView {
+    
+    var lastURLUsedToLoadImage: String?
+    
+    func loadImage(urlString: String) {
+        self.image = nil
+        //print("Loading image...")
+        lastURLUsedToLoadImage = urlString
+        
+        if let cachedImage = imageCache[urlString] {
+            //i += 1
+           // print("Cache Hit: \(i)")
+            self.image = cachedImage
+            return
+        }
+        j += 1
+        print("Cache Miss: \(j)")
+        guard let url = URL(string: urlString) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            if let err = err {
+                print("Failed to fetch post image:", err)
+                return
+            }
+            // because of reusing cell
+            if url.absoluteString != self.lastURLUsedToLoadImage {
+                return
+            }
+            guard let imageData = data else { return }
+            let photoImage = UIImage(data: imageData)
+            imageCache[url.absoluteString] = photoImage
+            DispatchQueue.main.async {
+                self.image = photoImage
+            }
+        }.resume()
+    }
+
+}
--- Instagram/Utilities/DataLoadOperation.swift
+++ Instagram/Utilities/DataLoadOperation.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import UIKit
+
+class DataPrefetchOperation: Operation {
+
+    let imageUrl: String
+    
+    init(with url:String) {
+        self.imageUrl = url
+    }
+    
+    override func main() {
+        if isCancelled { return }
+            // fire a download
+        ImageFetcherHelper.loadImage(with: self.imageUrl)
+    }
+}
+
+struct ImageFetcherHelper {
+    static func loadImage(with url: String) {
+        guard let url = URL(string: url) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            if let err = err {
+                print("Failed to fetch post image:", err)
+                return
+            }
+            guard let imageData = data else { return }
+            let photoImage = UIImage(data: imageData)
+            imageCache[url.absoluteString] = photoImage
+        }.resume()
+    }
+}
