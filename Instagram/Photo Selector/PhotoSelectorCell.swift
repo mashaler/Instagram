34be85490ff331fd0c49e7944e5ed200230e5570
mashaler <mashalerolva@gmail.com>
2024-09-06 21:03:21 +0200

    added photo selector functionality

--- Instagram/Photo Selector/PhotoSelectorCell.swift
+++ Instagram/Photo Selector/PhotoSelectorCell.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import UIKit
+
+class PhotoSelectorCell: UICollectionViewCell {
+    
+    let photoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .lightGray
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+
+    private func sharedInit() {
+        addSubview(photoImageView)
+        photoImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+    }
+
+}
